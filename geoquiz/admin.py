031fd3d91fbe011473ece095bad75f57efd14a2b
Alex Archer <archeralex@gmail.com>
2016-02-26 11:00:25 -0600

    Update admin.py to allow all model tables to be seen and edited through admin page URL

--- geoquiz/admin.py
+++ geoquiz/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
+from .models import Question, Quiz, Country, UserProfile
+
+admin.site.register(Question)
+admin.site.register(Quiz)
+admin.site.register(Country)
+admin.site.register(UserProfile)
